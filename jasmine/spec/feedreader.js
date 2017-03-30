a7fef8630f6a2ad7c5eb22cccaeb573e95dc50ff
Andrew Salcedo <asalcedo07@gmail.com>
2017-03-30 15:45:15 -0400

    Rss Feeds: pass spec--Name is defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -33,15 +33,20 @@ $(function() {
          */
          it('URL is defined and not empty', function() {
               allFeeds.forEach(function(feed) {
-                  expect(feed.url).toBeDefined(); 
+                  expect(feed.url).toBeDefined();
               });
          });
 
 
-        /* TODO: Write a test that loops through each feed
+        /*Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('Name is defined and not empty', function() {
+              allFeeds.forEach(function(feed) {
+                  expect(feed.name).toBeDefined();
+              });
+         });
     });
 
 
